d9e55f34790930017a6a9f3d09066edc598b3cf1
Ilya Timofeev <ilya@devim.team>
2017-10-26 16:11:13 +0300

    Add some stuff to General

--- src/Utils/General.elm
+++ src/Utils/General.elm
@@ -3,6 +3,8 @@ module Utils.General exposing (..)
 import FormatNumber.Locales exposing (Locale)
 import Date
 import Date.Format
+import Regex exposing (HowMany(..))
+import FormatNumber
 
 
 type Plural
@@ -23,16 +25,16 @@ pluralize : Plural -> Int -> String
 pluralize (Plural one two five) count =
     let
         n =
-            count % 100
+            count % 100 |> abs
 
         n10 =
-            count % 10
+            count % 10 |> abs
     in
         if n >= 5 && n <= 20 then
             five
         else if n10 == 1 then
             one
-        else if n10 >= 2 && n <= 4 then
+        else if n10 >= 2 && n10 <= 4 then
             two
         else
             five
@@ -70,3 +72,40 @@ formatTime dateString =
                     ""
     in
         result
+
+
+formatPhone : String -> String
+formatPhone phone =
+    let
+        combineParts : List String -> String
+        combineParts list =
+            (list |> getNthWithDefault 0 "")
+                ++ " ("
+                ++ (list |> getNthWithDefault 1 "")
+                ++ ") "
+                ++ (List.drop 2 list |> String.join "-")
+    in
+        Regex.replace
+            All
+            (Regex.regex "(^\\+\\d)(\\d{3})(\\d{3})(\\d{2})(.*)")
+            (\match ->
+                match.submatches
+                    |> List.map (\submatch -> Maybe.withDefault "" submatch)
+                    |> combineParts
+            )
+            phone
+
+
+getNth : Int -> List a -> Maybe a
+getNth n list =
+    List.head (List.drop n list)
+
+
+getNthWithDefault : Int -> a -> List a -> a
+getNthWithDefault n default list =
+    List.drop n list |> List.head |> Maybe.withDefault default
+
+
+formatMoney : Float -> String
+formatMoney amount =
+    FormatNumber.format rusLocale amount ++ " ₽"
